3b5344e94dcc35463c0aebe0cc7a90eb57db3411
tfogal <tfogal@sci.utah.edu>
2009-01-23 06:48:31 +0000

    Add an ifdef so we can use a dynamic Qt.
    The Q_IMPORT_PLUGIN macro causes issues if we're compiling against
    a shared Qt library.  This wraps the calls in a macro so we can
    easily switch to a shared build simply by adding a define.

--- IO/IV3D_QtPlugins.h
+++ IO/IV3D_QtPlugins.h
@@ -6,7 +6,7 @@
    Copyright (c) 2008 Scientific Computing and Imaging Institute,
    University of Utah.
 
-   
+
    Permission is hereby granted, free of charge, to any person obtaining a
    copy of this software and associated documentation files (the "Software"),
    to deal in the Software without restriction, including without limitation
@@ -37,8 +37,10 @@
 
 #include <QtCore/QtPlugin>
 
+#ifndef IV3D_DYNAMIC_QT
 Q_IMPORT_PLUGIN(qjpeg)
 Q_IMPORT_PLUGIN(qtiff)
 Q_IMPORT_PLUGIN(qgif)
+#endif // IV3D_DYNAMIC_QT
 
 #endif // IV3D_QTPLUGINS_H
