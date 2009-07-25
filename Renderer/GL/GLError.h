b7fda6ab66dd699a33587139abe8963ddde970bd
tfogal <tfogal@sci.utah.edu>
2009-07-25 22:42:49 +0000

    Add generic GLError.
    An exception which records the current GL error number.

--- Renderer/GL/GLError.h
+++ Renderer/GL/GLError.h
@@ -51,7 +51,7 @@ class OutOfMemory : virtual public std::runtime_error {
   public:
     explicit OutOfMemory(const char* e,
                          const char *where=NULL, size_t ln=0)
-      : runtime_error(e), error(e), location(where), line(ln) { }
+      : std::runtime_error(e), error(e), location(where), line(ln) { }
     virtual ~OutOfMemory() throw() { }
 
     virtual const char *what() const throw() { return this->error; }
@@ -69,6 +69,31 @@ class OutOfMemory : virtual public std::runtime_error {
 /// It automatically sets location information.
 #define OUT_OF_MEMORY(s) tuvok::OutOfMemory(s, __FILE__, __LINE__)
 
+/// Generic OpenGL error.
+class GLError : virtual public std::runtime_error {
+  public:
+    explicit GLError(int glerr,
+                     const char *where=NULL, size_t ln=0) :
+      std::runtime_error("OpenGL error"), glerrno(glerr), location(where),
+      line(ln) { }
+    virtual ~GLError() throw() { }
+
+    virtual const char *what() const throw() { return "OpenGL error"; }
+    int errno() const { return this->glerrno; }
+    const char *where() const { return this->location; }
+    size_t lineno() const { return this->line; }
+
+  private:
+    int glerrno;
+    const char *location;
+    size_t line;
 };
+
+/// Intended to be thrown, e.g.:
+///    throw OUT_OF_MEM("OpenGL 3d texture");
+/// It automatically sets location information.
+#define GL_ERROR(e) tuvok::GLError(e, __FILE__, __LINE__)
+
+}; // namespace
 
 #endif // TUVOK_GL_ERROR_H
