294e2ce7026c1c395dccdc52d658768ab74527e7
tfogal <tfogal@sci.utah.edu>
2010-05-18 22:26:52 +0000

    Apparently "NULL" doesn't count as a ptr in VStudio.

--- test/util-test.h
+++ test/util-test.h
@@ -55,7 +55,7 @@ inline void check_equality<double>(double a, double b) {
 static std::string mk_tmpfile(std::ofstream& ofs, std::ios_base::openmode mode)
 {
 #ifdef _WIN32
-  char *templ = tmpnam(NULL);
+  char *templ = tmpnam((char*)0);
   ofs.open(templ, mode);
 #else
   char templ[64];
