87768f763325814a942c70e33fbdc1fa7138700a
tfogal <tfogal@sci.utah.edu>
2010-11-23 23:36:13 +0000

    Derive exceptions from base tuvok exception.
    Unifying them allows clients to have a 'catch all problems that
    could arise from Tuvok' exception.

--- Renderer/GL/GLError.h
+++ Renderer/GL/GLError.h
@@ -38,8 +38,7 @@
 #ifndef TUVOK_GL_ERROR_H
 #define TUVOK_GL_ERROR_H
 
-#include <sstream>
-#include <stdexcept>
+#include "TuvokException.h"
 
 /// Defines OpenGL errors that we'll throw, and GL error detection code.
 
@@ -47,21 +46,12 @@ namespace tuvok {
 
 /// Thrown when allocating an OpenGL resource fails due to out of memory
 /// condition.
-class OutOfMemory : virtual public std::runtime_error {
+class OutOfMemory : virtual public tuvok::Exception {
   public:
     explicit OutOfMemory(const char* e,
                          const char *where=NULL, size_t ln=0)
-      : std::runtime_error(e), error(e), location(where), line(ln) { }
+      : tuvok::Exception(e, where, ln) { }
     virtual ~OutOfMemory() throw() { }
-
-    virtual const char *what() const throw() { return this->error; }
-    const char *where() const { return this->location; }
-    size_t lineno() const { return this->line; }
-
-  private:
-    const char *error;
-    const char *location;
-    size_t line;
 };
 
 /// Intended to be thrown, e.g.:
@@ -70,30 +60,24 @@ class OutOfMemory : virtual public std::runtime_error {
 #define OUT_OF_MEMORY(s) tuvok::OutOfMemory(s, __FILE__, __LINE__)
 
 /// Generic OpenGL error.
-class GLError : virtual public std::runtime_error {
+class GLError : virtual public tuvok::Exception {
   public:
     explicit GLError(int glerr,
                      const char *where=NULL, size_t ln=0) :
-      std::runtime_error("OpenGL error"), glerrno(glerr), location(where),
-      line(ln) { }
+      tuvok::Exception("OpenGL error", where, ln), glerrno(glerr) {}
     virtual ~GLError() throw() { }
 
-    virtual const char *what() const throw() { return "OpenGL error"; }
     int error() const { return this->glerrno; }
-    const char *where() const { return this->location; }
-    size_t lineno() const { return this->line; }
 
   private:
     int glerrno;
-    const char *location;
-    size_t line;
 };
+
+}
 
 /// Intended to be thrown, e.g.:
 ///    throw GL_ERROR(42);
 /// It automatically sets location information.
 #define GL_ERROR(e) tuvok::GLError(e, __FILE__, __LINE__)
-
-}; // namespace
 
 #endif // TUVOK_GL_ERROR_H
