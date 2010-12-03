b3cea7a3da229023a9e9f9a4112429fd763e15cf
tfogal <tfogal@sci.utah.edu>
2010-12-03 01:21:41 +0000

    Adjust for Qt 4.7

--- Tuvok_QtPlugins.h
+++ Tuvok_QtPlugins.h
@@ -41,23 +41,10 @@
 
 #ifndef TUVOK_NO_IO
   #ifndef QT_SHARED
-    Q_IMPORT_PLUGIN(qjpeg)
-    Q_IMPORT_PLUGIN(qtiff)
-    Q_IMPORT_PLUGIN(qgif)
-
-    #ifdef _WIN32
+    #if defined(_WIN32)
       #pragma comment( lib, "Imm32.lib" )
       #pragma comment( lib, "Winmm.lib" )
       #pragma comment( lib, "ws2_32.lib" )
-      #ifdef _DEBUG
-        #pragma comment( lib, "qjpegd.lib" )
-        #pragma comment( lib, "qtiffd.lib" )
-        #pragma comment( lib, "qgifd.lib" )
-      #else
-        #pragma comment( lib, "qjpeg.lib" )
-        #pragma comment( lib, "qtiff.lib" )
-        #pragma comment( lib, "qgif.lib" )
-      #endif
     #endif // _WIN32
   #endif // QT_SHARED
 #endif // TUVOK_NO_IO
