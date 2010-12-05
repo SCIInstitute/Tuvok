de0e5a343d5b179070313cbb7a49e0395929c9bc
tfogal <tfogal@sci.utah.edu>
2010-12-05 22:00:57 +0000

    3dtiffs: warn about an error condition.

--- TiffVolumeConverter.cpp
+++ TiffVolumeConverter.cpp
@@ -223,6 +223,11 @@ tv_dimensions(TIFF *tif, size_t dims[3])
   // tiff calls the height "length" for some reason.
   TIFFGetField(tif, TIFFTAG_IMAGELENGTH, &y);
   do {
+    uint32_t tmpx, tmpy;
+    TIFFGetField(tif, TIFFTAG_IMAGEWIDTH, &tmpx);
+    TIFFGetField(tif, TIFFTAG_IMAGELENGTH, &tmpy);
+    if(tmpx != x) { WARNING("TIFF x dimension changes in stack!"); }
+    if(tmpy != y) { WARNING("TIFF x dimension changes in stack!"); }
     ++z;
   } while(TIFFReadDirectory(tif));
   TIFFSetDirectory(tif, 0);
