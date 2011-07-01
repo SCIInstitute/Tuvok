0a907b91aba35b2b67e8a9d34c2a1777e5b9ae45
tfogal <tfogal@sci.utah.edu>
2011-07-01 20:56:06 +0000

    Fix windows comments.

--- scripts/oneshot-debug.bat
+++ scripts/oneshot-debug.bat
@@ -1,5 +1,5 @@
-; Script to build everything we can in a single invocation, using
-; a set of options which is appropriate for creating debug builds.
+REM Script to build everything we can in a single invocation, using
+REM a set of options which is appropriate for creating debug builds.
 
 set w32_cf="-D_CRT_SECURE_NO_WARNINGS=1 -D_SCL_SECURE_NO_WARNINGS=1"
 qmake -tp vc ^
@@ -8,7 +8,7 @@ qmake -tp vc ^
   QMAKE_LFLAGS+=%w32_cf% ^
   -recursive ^
   Tuvok.pro
-; hardcoding vs2008 for now =(
+REM hardcoding vs2008 for now =(
 bld="C:\Program Files (x86)\Microsoft Visual Studio 9.0\Common7\IDE\devenv.COM"
 %bld% ^
   Tuvok.sln ^
@@ -36,5 +36,5 @@ pushd IO\test
     /Rebuild
 popd
 
-; bundle it.
+REM bundle it.
 iscc Scripts/installer/64.iss
