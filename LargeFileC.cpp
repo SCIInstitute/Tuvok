c4b2e7353523fd7b3b7e1a1cb48dbce0b68cb251
tfogal <tfogal@sci.utah.edu>
2011-12-28 06:47:50 +0000

    lf: C streams (fopen/fread/write) based implementation.

---
+++ LargeFileC.cpp
@@ -0,0 +1,217 @@
+/*
+   For more information, please see: http://software.sci.utah.edu
+
+   The MIT License
+
+   Copyright (c) 2011 Scientific Computing and Imaging Institute,
+   University of Utah.
+
+
+   Permission is hereby granted, free of charge, to any person obtaining a
+   copy of this software and associated documentation files (the "Software"),
+   to deal in the Software without restriction, including without limitation
+   the rights to use, copy, modify, merge, publish, distribute, sublicense,
+   and/or sell copies of the Software, and to permit persons to whom the
+   Software is furnished to do so, subject to the following conditions:
+
+   The above copyright notice and this permission notice shall be included
+   in all copies or substantial portions of the Software.
+
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+   OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+   THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+   FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+   DEALINGS IN THE SOFTWARE.
+*/
+#include <cassert>
+#include <cstdio>
+#include <errno.h>
+#include <stdexcept>
+#if _POSIX_C_SOURCE >= 200112L
+# include <sys/types.h>
+#endif
+#ifndef NDEBUG
+# include <iostream>
+# define DEBUG(...) do { std::cerr << __VA_ARGS__ << "\n"; } while(0)
+#else
+# define DEBUG(...) do { /* nothing, debug msg removed. */ } while(0)
+#endif
+#include "LargeFileC.h"
+
+LargeFileC::LargeFileC(const std::string fn,
+                       std::ios_base::openmode mode,
+                       boost::uint64_t header_size,
+                       boost::uint64_t /* length */) :
+  LargeFile(fn, mode, header_size), fp(NULL)
+{
+  this->open(mode);
+}
+LargeFileC::LargeFileC(const std::wstring fn,
+                       std::ios_base::openmode mode,
+                       boost::uint64_t header_size,
+                       boost::uint64_t /* length */) :
+  LargeFile(fn, mode, header_size), fp(NULL)
+{
+  this->open(mode);
+}
+
+LargeFileC::~LargeFileC()
+{
+  if(this->is_open()) {
+    this->close();
+  }
+}
+
+namespace {
+  template<typename T> struct DeleteArray {
+    void operator()(const T* t) const { delete[] t; }
+  };
+}
+
+int seeko(FILE* strm, boost::uint64_t off, int whence) {
+#if _POSIX_C_SOURCE >= 200112L
+  if(fseeko(strm, off, whence) < 0) {
+#else
+  if(fseek(strm, off, whence) < 0) {
+#endif
+    DEBUG("seek failed, errno=" << errno);
+    return -1;
+  }
+  return 0;
+}
+
+std::tr1::shared_ptr<const void> LargeFileC::rd(boost::uint64_t offset,
+                                                size_t len)
+{
+  if(!this->is_open()) {
+    throw std::ios_base::failure("file is not open!");
+  }
+
+  if(seeko(this->fp, offset+this->header_size, SEEK_SET) < 0) {
+    throw std::ios_base::failure("could not seek to correct file position");
+  }
+
+  std::tr1::shared_ptr<char> data(new char[len], DeleteArray<char>());
+
+  size_t nitems = fread(data.get(), 1, len, this->fp);
+  this->bytes_read = nitems;
+
+  // we need to cast it to a 'const void' to return it...
+  std::tr1::shared_ptr<const void> mem =
+    std::tr1::static_pointer_cast<const void>(data);
+  return mem;
+}
+
+void LargeFileC::wr(const std::tr1::shared_ptr<const void>& data,
+                    boost::uint64_t offset,
+                    size_t len)
+{
+  if(!this->is_open()) {
+    throw std::ios_base::failure("file is not open!");
+  }
+
+  if(seeko(this->fp, offset+this->header_size, SEEK_SET) < 0) {
+    throw std::ios_base::failure("seek failed!");
+  }
+
+  if(fwrite(data.get(), len, 1, this->fp) <= 0) {
+    std::ios_base::failure("write failed.");
+  }
+}
+
+void LargeFileC::enqueue(boost::uint64_t, size_t)
+{
+  /* unimplemented... */
+}
+
+static boost::uint64_t offs(FILE* fp) {
+#if _POSIX_C_SOURCE >= 200112L
+  return static_cast<boost::uint64_t>(ftello(fp));
+#else
+  return static_cast<boost::uint64_t>(ftell(fp));
+#endif
+}
+
+boost::uint64_t LargeFileC::filesize() const
+{
+  if(!this->is_open()) {
+    throw std::ios_base::failure("file is not open");
+  }
+
+  const boost::uint64_t current = offs(this->fp); // save so we can reset
+  seeko(this->fp, 0, SEEK_END);
+  boost::uint64_t end = offs(this->fp);
+  seeko(this->fp, 0, SEEK_SET);
+  boost::uint64_t begin = offs(this->fp);
+  seeko(this->fp, current, SEEK_SET); // reset file pointer
+  return end - begin;
+}
+
+bool LargeFileC::is_open() const { return this->fp != NULL; }
+
+void LargeFileC::close()
+{
+  if(!LargeFileC::is_open()) { return; }
+
+  if(fclose(this->fp) != 0) {
+    assert(errno != EBADF);
+    if(errno == EBADF) {
+      throw std::invalid_argument("invalid file descriptor");
+    }
+    throw std::runtime_error("I/O error, writes might not have flushed!");
+  }
+  this->fp = NULL;
+}
+
+void LargeFileC::open(std::ios_base::openmode mode)
+{
+  if(this->is_open()) {
+    this->close();
+  }
+  const char* omode = NULL;
+  if((mode & std::ios::out) && (mode & std::ios::trunc)) {
+    omode = "w+b";
+  } else if(mode & std::ios::out) {
+    omode = "r+b";
+  } else {
+    omode = "rb";
+  }
+
+  this->fp = fopen(this->m_filename.c_str(), omode);
+  if(this->fp == NULL) {
+    throw std::ios_base::failure("Could not open file.");
+  }
+}
+
+int filenumber(FILE* strm) {
+#ifdef _MSC_VER
+  return _fileno(strm);
+#else
+  return fileno(strm);
+#endif
+}
+
+int lftruncate(int fd, boost::uint64_t length) {
+#ifdef _MSC_VER
+  return _chsize(fd, length);
+#else
+  return ftruncate(fd, length);
+#endif
+}
+
+void LargeFileC::truncate(boost::uint64_t len) {
+  if(!this->is_open()) {
+    this->open(std::ios::out);
+  }
+  int fno = filenumber(this->fp);
+
+  if(lftruncate(fno, len) != 0) {
+    DEBUG("truncate to len=" << len << " failed, errno=" << errno);
+    throw std::ios::failure("truncate failed");
+  }
+
+  // move the offset down if it's beyond EOF.
+  this->byte_offset = std::min(this->byte_offset, len);
+}
---
+++ LargeFileC.h
@@ -0,0 +1,78 @@
+/*
+   For more information, please see: http://software.sci.utah.edu
+
+   The MIT License
+
+   Copyright (c) 2011 Scientific Computing and Imaging Institute,
+   University of Utah.
+
+
+   Permission is hereby granted, free of charge, to any person obtaining a
+   copy of this software and associated documentation files (the "Software"),
+   to deal in the Software without restriction, including without limitation
+   the rights to use, copy, modify, merge, publish, distribute, sublicense,
+   and/or sell copies of the Software, and to permit persons to whom the
+   Software is furnished to do so, subject to the following conditions:
+
+   The above copyright notice and this permission notice shall be included
+   in all copies or substantial portions of the Software.
+
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+   OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+   THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+   FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+   DEALINGS IN THE SOFTWARE.
+*/
+#ifndef BASICS_LARGEFILE_C_H
+#define BASICS_LARGEFILE_C_H
+
+#include <cstdio>
+#include "LargeFile.h"
+
+/** Uses standard C API calls (fopen/fread/fwrite) as the backing impl. */
+class LargeFileC : public LargeFile {
+  public:
+    /// @argument header_size is maintained as a "base" offset.  Seeking to
+    /// byte 0 actually seeks to 'header_size'.
+    LargeFileC(const std::string fn,
+               std::ios_base::openmode mode = std::ios_base::in,
+               boost::uint64_t header_size=0,
+               boost::uint64_t length=0);
+    /// @argument header_size is maintained as a "base" offset.  Seeking to
+    /// byte 0 actually seeks to 'header_size'.
+    LargeFileC(const std::wstring fn,
+               std::ios_base::openmode mode = std::ios_base::in,
+               boost::uint64_t header_size=0,
+               boost::uint64_t length=0);
+    virtual ~LargeFileC();
+
+    virtual void open(std::ios_base::openmode mode = std::ios_base::in);
+
+    /// reads a block of data, returns a pointer to it.  User must cast it to
+    /// the type that makes sense for them.
+    virtual std::tr1::shared_ptr<const void> rd(boost::uint64_t offset,
+                                                size_t len);
+    using LargeFile::read;
+    using LargeFile::rd;
+
+    /// writes a block of data.
+    virtual void wr(const std::tr1::shared_ptr<const void>& data,
+                    boost::uint64_t offset,
+                    size_t len);
+
+    /// notifies the object that we're going to need the following data soon.
+    /// Many implementations will prefetch this data when it knows this.
+    virtual void enqueue(boost::uint64_t offset, size_t len);
+
+    virtual boost::uint64_t filesize() const;
+    virtual bool is_open() const;
+    virtual void close();
+    virtual void truncate(boost::uint64_t length=0);
+
+  protected:
+    FILE* fp;
+};
+
+#endif /* BASICS_LARGEFILE_C_H */
