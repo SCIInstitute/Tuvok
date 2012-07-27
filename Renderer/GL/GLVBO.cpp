0339be45929b65c9d8d5752471d2f6d2641390c8
jens <jens@sci.utah.edu>
2012-07-27 14:02:07 +0000

    realized VBO class, which is meant to replace immediate-mode calls (i.e. glBegin() ... glEnd())
    Note for users:
    The implementation (with the generic vertex attribute types) is targeting a gl "core" implementation that avoid any old-style attribute semantics (e.g. gl_normals, gl_texcoords). To access such attribute arrays in the shader use the "layout location" parameter of input variables, e.g., for a 2D position bound to location 0 the shader code looks like this:
    
    layout (location=0) in vec2 MyVertexPosition;

---
+++ Renderer/GL/GLVBO.cpp
@@ -0,0 +1,174 @@
+#include "GLVBO.h"
+
+#include <GL/glew.h>
+#include "GLInclude.h"
+
+using namespace tuvok;
+
+GLVBO::GLVBO() :
+  m_vboHandles(0),
+  m_iboHandle(std::make_pair(IBDesc(0,0),0))
+{
+}
+
+GLVBO::~GLVBO() {
+  FreeGL();
+}
+
+GLuint GLVBO::GetNewAB() {
+  GLuint vboHandle = 0;
+  GL(glGenBuffers(1, &vboHandle));
+  GL(glBindBuffer(GL_ARRAY_BUFFER, vboHandle));
+  return vboHandle;
+}
+
+void GLVBO::FreeGL() {
+  if(m_vboHandles.size()) {
+    for (auto buffer = m_vboHandles.begin();buffer<m_vboHandles.end();buffer++) 
+      GL(glDeleteBuffers(1, &(buffer->second)));
+    m_vboHandles.clear();
+  }
+  if(m_iboHandle.first.count) {
+    GL(glDeleteBuffers(1, &(m_iboHandle.second)));
+    m_iboHandle.first.count = 0;
+    m_iboHandle.first.type = 0;
+    m_iboHandle.second = 0;
+  }
+}
+
+void GLVBO::Bind() const {
+  GLuint i = 0;
+  for (auto buffer = m_vboHandles.begin();buffer<m_vboHandles.end();buffer++) {
+    GL(glBindBuffer(GL_ARRAY_BUFFER, buffer->second));
+    GL(glEnableVertexAttribArray(i));
+    GL(glVertexAttribPointer(i++, GLint(buffer->first.elemCount), buffer->first.type, GL_FALSE, 0, NULL));
+  }
+  glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_iboHandle.second); 
+}
+
+void GLVBO::Draw(GLenum mode) const {
+  if (m_vboHandles.empty()) return;
+  
+  if(m_iboHandle.first.count)
+    GL(glDrawElements(mode, GLsizei(m_iboHandle.first.count), m_iboHandle.first.type, 0));
+  else
+    GL(glDrawArrays(mode, 0, GLsizei(m_vboHandles[0].first.count)));
+}
+
+void GLVBO::Draw(GLsizei count, GLenum mode) const {
+  if (m_vboHandles.empty()) return;
+
+  if(m_iboHandle.first.count)
+    GL(glDrawElements(mode, count, m_iboHandle.first.type, 0));
+  else
+    GL(glDrawArrays(mode, 0, count));
+}
+
+void GLVBO::UnBind() const {
+  GLuint i = 0;
+  for (auto buffer = m_vboHandles.begin();buffer<m_vboHandles.end();buffer++) {
+    GL(glDisableVertexAttribArray(i++));
+  }
+  GL(glBindBuffer(GL_ARRAY_BUFFER, 0));
+  GL(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0)); 
+}
+
+uint64_t GLVBO::GetCPUSize() const {
+  return GetGPUSize();
+}
+
+uint64_t GLVBO::GetGPUSize() const {
+  return 0;
+}
+
+void GLVBO::SetIndexData(const std::vector<uint32_t>& indexData) {
+  if (indexData.empty()) return;
+
+  if (m_iboHandle.first.count == 0) GL(glGenBuffers(1, &m_iboHandle.second));
+  m_iboHandle.first.type = GL_UNSIGNED_INT;
+  m_iboHandle.first.count = indexData.size();
+  GL(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_iboHandle.second));
+  GL(glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(uint32_t)*indexData.size(), &(indexData[0]), GL_STATIC_DRAW));
+}
+
+void GLVBO::SetIndexData(const std::vector<uint16_t>& indexData) {
+  if (indexData.empty()) return;
+
+  if (m_iboHandle.first.count == 0) GL(glGenBuffers(1, &m_iboHandle.second));
+  m_iboHandle.first.type = GL_UNSIGNED_SHORT;
+  m_iboHandle.first.count = indexData.size();
+  GL(glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, m_iboHandle.second));
+  GL(glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(uint16_t)*indexData.size(), &(indexData[0]), GL_STATIC_DRAW));
+}
+
+void GLVBO::AddVertexData(const std::vector<float>& vertexData) {
+  AddVertexData(1, sizeof(float), GL_FLOAT, vertexData.size(), &(vertexData[0]));
+}
+void GLVBO::AddVertexData(const std::vector<FLOATVECTOR2>& vertexData){
+  AddVertexData(2, sizeof(float), GL_FLOAT, vertexData.size(), &(vertexData[0]));
+}
+void GLVBO::AddVertexData(const std::vector<FLOATVECTOR3>& vertexData){
+  AddVertexData(3, sizeof(float), GL_FLOAT, vertexData.size(), &(vertexData[0]));
+}
+void GLVBO::AddVertexData(const std::vector<FLOATVECTOR4>& vertexData){
+  AddVertexData(4, sizeof(float), GL_FLOAT, vertexData.size(), &(vertexData[0]));
+}
+
+void GLVBO::AddVertexData(const std::vector<int32_t>& vertexData){
+  AddVertexData(1, sizeof(int32_t), GL_INT, vertexData.size(), &(vertexData[0]));
+}
+void GLVBO::AddVertexData(const std::vector<INTVECTOR2>& vertexData){
+  AddVertexData(2, sizeof(int32_t), GL_INT, vertexData.size(), &(vertexData[0]));
+}
+void GLVBO::AddVertexData(const std::vector<INTVECTOR3>& vertexData){
+  AddVertexData(3, sizeof(int32_t), GL_INT, vertexData.size(), &(vertexData[0]));
+}
+void GLVBO::AddVertexData(const std::vector<INTVECTOR4>& vertexData){
+  AddVertexData(4, sizeof(int32_t), GL_INT, vertexData.size(), &(vertexData[0]));
+}
+
+void GLVBO::AddVertexData(const std::vector<uint32_t>& vertexData){
+  AddVertexData(1, sizeof(uint32_t), GL_UNSIGNED_INT, vertexData.size(), &(vertexData[0]));
+}
+void GLVBO::AddVertexData(const std::vector<UINTVECTOR2>& vertexData){
+  AddVertexData(2, sizeof(uint32_t), GL_UNSIGNED_INT, vertexData.size(), &(vertexData[0]));
+}
+void GLVBO::AddVertexData(const std::vector<UINTVECTOR3>& vertexData){
+  AddVertexData(3, sizeof(uint32_t), GL_UNSIGNED_INT, vertexData.size(), &(vertexData[0]));
+}
+void GLVBO::AddVertexData(const std::vector<UINTVECTOR4>& vertexData){
+  AddVertexData(4, sizeof(uint32_t), GL_UNSIGNED_INT, vertexData.size(), &(vertexData[0]));
+}
+
+void GLVBO::AddVertexData(const std::vector<int8_t>& vertexData){
+  AddVertexData(1, sizeof(int8_t), GL_BYTE, vertexData.size(), &(vertexData[0]));
+}
+void GLVBO::AddVertexData(const std::vector< VECTOR2<int8_t> >& vertexData){
+  AddVertexData(2, sizeof(int8_t), GL_BYTE, vertexData.size(), &(vertexData[0]));
+}
+void GLVBO::AddVertexData(const std::vector< VECTOR3<int8_t> >& vertexData){
+  AddVertexData(3, sizeof(int8_t), GL_BYTE, vertexData.size(), &(vertexData[0]));
+}
+void GLVBO::AddVertexData(const std::vector< VECTOR4<int8_t> >& vertexData){
+  AddVertexData(4, sizeof(int8_t), GL_BYTE, vertexData.size(), &(vertexData[0]));
+}
+
+void GLVBO::AddVertexData(const std::vector<uint8_t>& vertexData){
+  AddVertexData(1, sizeof(int8_t), GL_UNSIGNED_BYTE, vertexData.size(), &(vertexData[0]));
+}
+void GLVBO::AddVertexData(const std::vector< VECTOR2<uint8_t> >& vertexData){
+  AddVertexData(2, sizeof(int8_t), GL_UNSIGNED_BYTE, vertexData.size(), &(vertexData[0]));
+}
+void GLVBO::AddVertexData(const std::vector< VECTOR3<uint8_t> >& vertexData){
+  AddVertexData(3, sizeof(int8_t), GL_UNSIGNED_BYTE, vertexData.size(), &(vertexData[0]));
+}
+void GLVBO::AddVertexData(const std::vector< VECTOR4<uint8_t> >& vertexData){
+  AddVertexData(4, sizeof(int8_t), GL_UNSIGNED_BYTE, vertexData.size(), &(vertexData[0]));
+}
+
+void GLVBO::AddVertexData(size_t elemCount, size_t elemSize, GLenum type, size_t count, const GLvoid * pointer) {
+  GLuint id = GetNewAB();
+  m_vboHandles.push_back(std::make_pair(ABDesc(type, elemCount, count), id));
+  GL(glBufferData(GL_ARRAY_BUFFER, count*elemCount*elemSize, pointer, GL_STATIC_DRAW));
+}
+
---
+++ Renderer/GL/GLVBO.h
@@ -0,0 +1,116 @@
+#pragma once
+
+#ifndef GLVBO_H
+#define GLVBO_H
+
+#include "StdTuvokDefines.h"
+#include "GLObject.h"
+#include "Basics/Vectors.h"
+#include <memory>
+
+namespace tuvok {
+
+  class ABDesc {
+  public:
+    ABDesc() {}
+    ABDesc(GLenum _type, size_t _elemCount, size_t _count) :
+      type(_type),
+      elemCount(_elemCount),
+      count(_count) {}
+
+    GLenum type;
+    size_t elemCount;
+    size_t count;
+  };
+
+  class IBDesc {
+  public:
+    IBDesc() {}
+    IBDesc(GLenum _type, size_t _count) :
+      type(_type),
+      count(_count) {}
+
+    GLenum type;
+    size_t count;
+  };  
+
+  class GLVBO : public GLObject {
+    public:
+      GLVBO();
+      virtual ~GLVBO();
+
+      void AddVertexData(const std::vector<float>& vertexData);
+      void AddVertexData(const std::vector<FLOATVECTOR2>& vertexData);
+      void AddVertexData(const std::vector<FLOATVECTOR3>& vertexData);
+      void AddVertexData(const std::vector<FLOATVECTOR4>& vertexData);
+
+      void AddVertexData(const std::vector<int32_t>& vertexData);
+      void AddVertexData(const std::vector<INTVECTOR2>& vertexData);
+      void AddVertexData(const std::vector<INTVECTOR3>& vertexData);
+      void AddVertexData(const std::vector<INTVECTOR4>& vertexData);
+
+      void AddVertexData(const std::vector<uint32_t>& vertexData);
+      void AddVertexData(const std::vector<UINTVECTOR2>& vertexData);
+      void AddVertexData(const std::vector<UINTVECTOR3>& vertexData);
+      void AddVertexData(const std::vector<UINTVECTOR4>& vertexData);
+
+      void AddVertexData(const std::vector<int8_t>& vertexData);
+      void AddVertexData(const std::vector< VECTOR2<int8_t> >& vertexData);
+      void AddVertexData(const std::vector< VECTOR3<int8_t> >& vertexData);
+      void AddVertexData(const std::vector< VECTOR4<int8_t> >& vertexData);
+
+      void AddVertexData(const std::vector<uint8_t>& vertexData);
+      void AddVertexData(const std::vector< VECTOR2<uint8_t> >& vertexData);
+      void AddVertexData(const std::vector< VECTOR3<uint8_t> >& vertexData);
+      void AddVertexData(const std::vector< VECTOR4<uint8_t> >& vertexData);
+
+      void SetIndexData(const std::vector<uint16_t>& indexData);
+      void SetIndexData(const std::vector<uint32_t>& indexData);
+
+      void FreeGL();
+
+      void Bind() const;
+      void Draw(GLsizei count, GLenum mode) const;
+      void Draw(GLenum mode) const;
+      void UnBind() const;
+
+      virtual uint64_t GetCPUSize() const;
+      virtual uint64_t GetGPUSize() const;
+
+    private:
+      std::vector<std::pair<ABDesc, GLuint>> m_vboHandles;
+      std::pair<IBDesc, GLuint> m_iboHandle;
+
+      GLuint GetNewAB();
+      void AddVertexData(size_t elemCount, size_t elemSize, GLenum type, size_t count, const GLvoid * pointer);
+  };
+
+}
+
+#endif // GLVBO_H
+
+/*
+   The MIT License
+
+   Copyright (c) 2012 Interactive Visualization and Data Analysis Group,
+   Saarland University
+
+
+   Permission is hereby granted, free of charge, to any person obtaining a
+   copy of this software and associated documentation files (the "Software"),
+   to deal in the Software without restriction, including without limitation
+   the rights to use, copy, modify, merge, publish, distribute, sublicense,
+   and/or sell copies of the Software, and to permit persons to whom the
+   Software is furnished to do so, subject to the following conditions:
+
+   The above copyright notice and this permission notice shall be included
+   in all copies or substantial portions of the Software.
+
+   THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS
+   OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+   FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL
+   THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+   LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING
+   FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER
+   DEALINGS IN THE SOFTWARE.
+*/
