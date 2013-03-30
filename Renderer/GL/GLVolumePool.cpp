39db00c9db986610004b628a21897cb199db84f6
alex <alex>
2013-03-30 12:22:27 +0000

    do the real percentage thing

--- Renderer/GL/GLVolumePool.cpp
+++ Renderer/GL/GLVolumePool.cpp
@@ -1622,9 +1622,9 @@ UINTVECTOR3 GLVolumePool::RecomputeVisibility(VisibilityState const& visibility,
     MESSAGE("Synchronously recomputed brick visibility: %u bricks processed, "
             "%u bricks are EMPTY (%.2f%%), %u bricks are CHILD_EMPTY (%.2f%%)",
             vEmptyBrickCount.x, vEmptyBrickCount.y,
-            (static_cast<float>(vEmptyBrickCount.y)/vEmptyBrickCount.x)+100.0f,
+            (static_cast<float>(vEmptyBrickCount.y)/vEmptyBrickCount.x)*100.0f,
             vEmptyBrickCount.z,
-            (static_cast<float>(vEmptyBrickCount.z)/vEmptyBrickCount.x)+100.0f);
+            (static_cast<float>(vEmptyBrickCount.z)/vEmptyBrickCount.x)*100.0f);
   }
 
   // upload new metadata to GPU
