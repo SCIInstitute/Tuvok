0d459dacbce00526c57a7f73e715bd9f0d0a6cc8
tfogal <tfogal@sci.utah.edu>
2013-03-31 16:33:08 +0000

    bcache: test for adding many elements

--- test/bcache.h
+++ test/bcache.h
@@ -2,6 +2,8 @@
 #include <algorithm>
 #include <cxxtest/TestSuite.h>
 #include "BrickCache.h"
+#include "Controller/Controller.h"
+#include "util-test.h"
 
 using namespace tuvok;
 // just make the object!
@@ -92,6 +94,46 @@ void sizes() {
   TS_ASSERT_EQUALS(c.size(), 0U);
 }
 
+namespace {
+  template<typename T>
+  void normal(std::vector<T>& data, const T& mean, const T& stddev) {
+    // double: RNGs are only defined for FP types.  We'll generate double
+    // and just cast to T.
+    std::random_device genSeed;
+    std::mt19937 mtwister(genSeed());
+    std::normal_distribution<double> normalDist(mean, stddev);
+    std::function<double()> getRandNormal(std::bind(normalDist, mtwister));
+
+    for(size_t i=0; i < data.size(); ++i) {
+      T v = static_cast<T>(getRandNormal());
+      data[i] = v;
+    }
+  }
+}
+
+// this is really a benchmark, not a test per se...
+void add_many() {
+  BrickCache c;
+  std::vector<uint8_t> src(68*68*68);
+  normal<uint8_t>(src, uint8_t(64), uint8_t(12));
+
+  EnableDebugMessages edm;
+  BrickKey key(0,0,0);
+  for(size_t i=0; i < 2048; ++i) {
+    std::vector<uint8_t> buf(68*68*68);
+    std::copy(src.begin(), src.end(), buf.begin());
+    key = std::make_tuple(0U, 0U, i);
+    c.add(key, buf);
+  }
+  double cache_add = Controller::Instance().PerfQuery(PERF_CACHE_ADD);
+  double cache_lookup = Controller::Instance().PerfQuery(PERF_CACHE_LOOKUP);
+  double something = Controller::Instance().PerfQuery(PERF_SOMETHING);
+  double bcopy = Controller::Instance().PerfQuery(PERF_BRICK_COPY);
+  fprintf(stderr, "\ncache add: %g\ncache lookup: %g\bcopy: %g\n"
+          "something: %g\n",
+          cache_add, cache_lookup, bcopy, something);
+}
+
 class BCacheTests : public CxxTest::TestSuite {
 public:
   void test_simple() { simple(); }
@@ -100,4 +142,5 @@ public:
   void test_sizes() { sizes(); }
   void test_lookup_bug() { lookup_bug(); }
   void test_lookup_bug16() { lookup_bug16(); }
+//  void test_add_many() { add_many(); }
 };
