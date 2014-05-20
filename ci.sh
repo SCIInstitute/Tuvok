d1cbdd336c12321c98ecd6f308e0da2a969c1e41
Tom Fogal <tfogal@alumni.unh.edu>
2014-05-20 11:44:03 +0200

    add a build script, ostensibly for CI.

---
+++ ci.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+VIS="-fvisibility=hidden"
+CF="-ggdb3 -Wall -Wextra -O0 -D_DEBUG -D_GLIBCXX_DEBUG -fstack-protector"
+CXF="-D_GLIBCXX_CONCEPT_CHECK -fvisibility-inlines-hidden"
+qmake \
+  CONFIG+="debug" \
+  QMAKE_CFLAGS+="${CF}" \
+  QMAKE_CXXFLAGS+="${VIS} ${CF} ${CXF}" \
+  -recursive \
+  Tuvok.pro || exit 1
+nice make -j2
