abb5a3d46728c6800aae7e4f221aeb11e29553f7
Tom Fogal <tfogal@alumni.unh.edu>
2014-05-20 12:29:13 +0200

    ci: use mpic++ to compile.

--- ci.sh
+++ ci.sh
@@ -5,6 +5,7 @@ CF="-ggdb3 -Wall -Wextra -O0 -D_DEBUG -D_GLIBCXX_DEBUG -fstack-protector"
 CXF="-D_GLIBCXX_CONCEPT_CHECK -fvisibility-inlines-hidden"
 qmake \
   CONFIG+="debug" \
+  QMAKE_CXX="mpic++" \
   QMAKE_CFLAGS+="${CF}" \
   QMAKE_CXXFLAGS+="${VIS} ${CF} ${CXF}" \
   -recursive \
